5f99ff5c6c07746cb7c3d6041f7ac2a0437d3080
aolmenki <emeril95@gmail.com>
2019-01-23 22:12:09 -0500

    feat: Implement JSON Parser
    Added string literals for JSON keys to strings.xml and updated the method
    to take the Application Context so the JsonUtils Object could access them.
    
    JSON Parser is fully implmented and documented.
    
    Resolves:
    JSON data is parsed correctly to a Sandwich Object
    JSON is parsed without using 3rd party libraries

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -36,7 +36,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = JsonUtils.parseSandwichJson(json, getApplicationContext());
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,63 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json, Context p_context) {
+        Sandwich returnSandwich = new Sandwich();
+
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            // Sandwich Names
+            JSONObject jsonName = jsonSandwich.getJSONObject(p_context.getString(R.string.json_key_name));
+
+            // Sandwich Main Name
+            String strMainName = jsonName.getString(p_context.getString(R.string.json_key_mainName));
+            returnSandwich.setMainName(strMainName);
+
+            // Sandwich Also Known As
+            JSONArray jsonArrAKA = jsonName.getJSONArray(p_context.getString(R.string.json_key_alsoKnownAs));
+            List<String> listAKA = new ArrayList<>();
+            for (int i = 0; i < jsonArrAKA.length(); i++) {
+                listAKA.add(jsonArrAKA.getString(i));
+            }
+            returnSandwich.setAlsoKnownAs(listAKA);
+
+            // Sandwich Origin
+            String strOrigin = jsonSandwich.getString(p_context.getString(R.string.json_key_placeOfOrigin));
+            returnSandwich.setPlaceOfOrigin(strOrigin);
+
+            // Sandwich Description
+            String strDesc = jsonSandwich.getString(p_context.getString(R.string.json_key_description));
+            returnSandwich.setDescription(strDesc);
+
+            // Sandwich Image
+            String strImg = jsonSandwich.getString(p_context.getString(R.string.json_key_image));
+            returnSandwich.setImage(strImg);
+
+            // Sandwich Ingredients
+            JSONArray jsonArrIngd = jsonName.getJSONArray(p_context.getString(R.string.json_key_ingredients));
+            List<String> listIngd = new ArrayList<>();
+            for (int i = 0; i < jsonArrIngd.length(); i++) {
+                listIngd.add(jsonArrIngd.getString(i));
+            }
+            returnSandwich.setIngredients(listIngd);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return returnSandwich;
     }
 }
